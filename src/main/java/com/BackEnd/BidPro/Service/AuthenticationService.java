27242cdc694a5ecb432a5d3964576ffa03bd588d
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2025-03-01 17:43:28 +0200

    Hello there

--- src/main/java/com/BackEnd/BidPro/Service/AuthenticationService.java
+++ src/main/java/com/BackEnd/BidPro/Service/AuthenticationService.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+//Alo
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
