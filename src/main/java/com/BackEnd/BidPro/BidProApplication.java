fab605a00e9ad0be33066869c2dc399b3d7326e1
Mohamedyasser02 <mohammedyasser0031@gmail.com>
2025-04-07 17:59:59 +0200

    some features for chat homies

--- src/main/java/com/BackEnd/BidPro/BidProApplication.java
+++ src/main/java/com/BackEnd/BidPro/BidProApplication.java
@@ -3,6 +3,11 @@ package com.BackEnd.BidPro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.SimpleTimeZone;
+
 @SpringBootApplication
 public class BidProApplication {
 
--- src/main/java/com/BackEnd/BidPro/chat/service/RoomServiceImpl.java
+++ src/main/java/com/BackEnd/BidPro/chat/service/RoomServiceImpl.java
@@ -10,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
@@ -39,7 +42,8 @@ public class RoomServiceImpl implements RoomService {
         nameOfRoom.append("Admin, group chat");
         room.setName(nameOfRoom.toString());
         room.setUsers(users);
-        room.setCreationDate(new Date());
+        ZonedDateTime cairoTime = ZonedDateTime.now();
+        room.setCreationDate(cairoTime);
         roomRepository.save(room);
     }
 
@@ -50,11 +54,32 @@ public class RoomServiceImpl implements RoomService {
                 .orElseThrow(() -> new RuntimeException("Please provide an valid Email!"));
         List<Room> rooms = roomRepository.findRoomsByUser(user.getId());
         List<RoomResponse> roomResponses = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        ZonedDateTime lastMsgTime;
         for(Room room : rooms) {
+            ZonedDateTime time;
             RoomResponse roomResponse = new RoomResponse();
             roomResponse.setId(room.getId());
             roomResponse.setName(room.getName());
-            roomResponse.setCreationDate(room.getCreationDate());
+            time = room.getCreationDate().toInstant().atZone(ZoneId.of("Africa/Cairo"));
+            roomResponse.setCreationDate(time.format(formatter));
+            if(room.getMessages().size()>=1){
+                int size = room.getMessages().size();
+                roomResponse.setLastMessage(room.getMessages().get(size-1).getMessage());
+                lastMsgTime = room.getMessages().get(size-1).getMessageDate().toInstant().atZone(ZoneId.of("Africa/Cairo"));
+                roomResponse.setLastMessageDate(lastMsgTime.format(formatter));
+                if((room.getMessages().get(size - 1).getSender().getRole().name().equals("ADMIN"))){
+                    roomResponse.setLastMessageUser("Admin");
+                }
+                else {
+                    roomResponse.setLastMessageUser(room.getMessages().get(size - 1).getSender().getName());
+                }
+            }
+            else {
+                roomResponse.setLastMessageUser(null);
+                roomResponse.setLastMessageDate(null);
+                roomResponse.setLastMessage(null);
+            }
             roomResponses.add(roomResponse);
         }
         return roomResponses;
